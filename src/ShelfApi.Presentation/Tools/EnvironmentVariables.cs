f7642c81a86906ef35ecc0b9c74e075281268ceb
soroushkavousi <soroushkavousi.me@gmail.com>
2025-01-24 12:02:53 +0330

    refactor: add EnvironmentHelper to the Shared project

--- src/ShelfApi.Presentation/Tools/EnvironmentVariables.cs
+++ src/ShelfApi.Presentation/Tools/EnvironmentVariables.cs
@@ -1,28 +1,12 @@
-﻿namespace ShelfApi.Presentation.Tools;
+﻿using Bitiano.Shared.Helpers;
+
+namespace ShelfApi.Presentation.Tools;
 
 public static class EnvironmentVariables
 {
-    public static string ConnectionString { get; } = ReadEnvironmentValue
+    public static string ConnectionString { get; } = EnvironmentHelper.ReadVariable
     (
         key: "SHELF_API_CONNECTION_STRING",
         defaultValue: null
     );
-
-    public static string ReadEnvironmentValue(string key, string defaultValue = null)
-    {
-        string value = Environment.GetEnvironmentVariable(key, EnvironmentVariableTarget.Process);
-        if (!string.IsNullOrWhiteSpace(value))
-            return value;
-
-        value = Environment.GetEnvironmentVariable(key, EnvironmentVariableTarget.Machine);
-        if (!string.IsNullOrWhiteSpace(value))
-            return value;
-
-        value = Environment.GetEnvironmentVariable(key, EnvironmentVariableTarget.User);
-        if (!string.IsNullOrWhiteSpace(value))
-            return value;
-
-        value = defaultValue;
-        return value;
-    }
 }
